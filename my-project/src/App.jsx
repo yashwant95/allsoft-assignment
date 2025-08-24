1cd8f6e17702f6c3e56fbe27660ebec202d1bc25
yashwant dewangan <yashwantdewangan1234@gmail.com>
2025-08-24 13:52:04 +0530

    Add login page
    Introduces a new Login page with OTP-based authentication and an admin interface

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Login from './pages/Login';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className='text-red-900'>Hello World</div>
-    </>
-  )
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
-export default App
+export default App;
